8c7e9b4ef8a03ea08ef238cac73bfdc856c9fc7c
Moxie03 <oluwatobilobatolulope@gmail.com>
2022-07-22 11:28:37 -0700

    fix

---
+++ 0x09-static_libraries/0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0-9 except 2&4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
+	}
+
+	_putchar('\n');
+}
